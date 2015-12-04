39ae37280ca9f8032370e8bb943ac0ff90ecdf77
Sam Hatoum <sam@xolv.io>
2015-12-04 00:38:54 -0800

    Adds missing files and configures ignores

---
+++ bin/whirlwind.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var fs = require('fs');
+var nodeController = require('./../lib/node-controller');
+
+var config = JSON.parse(fs.readFileSync('whirlwind.json', 'utf8'));
+nodeController.run(config);
