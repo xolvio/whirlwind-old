888df85742b2ec1bc9c1b199a8276d5e6b04065b
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-12-18 10:06:18 +0100

    Fixes tests

--- src/spec/exec-runner-spec.js
+++ src/spec/exec-runner-spec.js
@@ -8,7 +8,11 @@ describe('Exec Runner', function () {
       execRunner.createRunner({
         command: 'command $TASKS',
       })(['task-one', 'task-two']);
-      expect(childProcess.exec).toHaveBeenCalledWith('command task-one,task-two', jasmine.any(Function));
+      expect(childProcess.exec).toHaveBeenCalledWith(
+        'command task-one,task-two',
+        undefined,
+        jasmine.any(Function)
+      );
     });
     it('flattens task arrays into the command line using a the provided separator', function () {
       spyOn(childProcess, 'exec');
@@ -16,20 +20,28 @@ describe('Exec Runner', function () {
         command: 'command $TASKS',
         separator: '#',
       })(['task-one', 'task-two', 'task-three']);
-      expect(childProcess.exec).toHaveBeenCalledWith('command task-one#task-two#task-three', jasmine.any(Function));
+      expect(childProcess.exec).toHaveBeenCalledWith(
+        'command task-one#task-two#task-three',
+        undefined,
+        jasmine.any(Function)
+      );
     });
     it('creates a command from a single string', function () {
       spyOn(childProcess, 'exec');
       execRunner.createRunner({
         command: 'command $TASKS',
       })('single-task');
-      expect(childProcess.exec).toHaveBeenCalledWith('command single-task', jasmine.any(Function));
+      expect(childProcess.exec).toHaveBeenCalledWith(
+        'command single-task',
+        undefined,
+        jasmine.any(Function)
+      );
     });
     it('delegates errors and results up the callbacks chain', function () {
       spyOn(childProcess, 'exec');
       const callbackChain = jasmine.createSpy();
       execRunner.createRunner({command: 'command'})(null, callbackChain);
-      const execCallback = childProcess.exec.calls.argsFor(0)[1];
+      const execCallback = childProcess.exec.calls.argsFor(0)[2];
 
       execCallback('error', null);
       execCallback(null, 'results');
