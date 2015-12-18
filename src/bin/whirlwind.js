5a57f7daf4bc79452f93516030dc25edc5482142
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-12-18 10:03:33 +0100

    Exit whirlwind correctly

--- src/bin/whirlwind.js
+++ src/bin/whirlwind.js
@@ -2,6 +2,9 @@
 
 const fs = require('fs');
 const nodeController = require('./../lib/node-controller');
+const exit = require('exit');
 
 const config = JSON.parse(fs.readFileSync('whirlwind.json', 'utf8'));
-nodeController.run(config);
+nodeController.run(config, function onFinish(exitCode) {
+  exit(exitCode);
+});
